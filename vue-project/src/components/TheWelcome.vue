37dc2db34b8c7dd9e140f6e9af98615ed871d276
Iuri Fiedoruk <protoman@upperland.net>
2024-06-28 10:12:24 -0300

    Initial import

---
+++ vue-project/src/components/TheWelcome.vue
@@ -0,0 +1,38 @@
+<script setup lang="ts">
+import WelcomeItem from './WelcomeItem.vue'
+import DocumentationIcon from './icons/IconDocumentation.vue'
+import ToolingIcon from './icons/IconTooling.vue'
+import EcosystemIcon from './icons/IconEcosystem.vue'
+</script>
+
+<template>
+  <WelcomeItem>
+    <template #icon>
+      <DocumentationIcon />
+    </template>
+    <template #heading>Projects</template>
+
+    <router-link :to="{ name: 'listProjects' }" replace>List Projects #1</router-link><br />
+    <a href="https://vuejs.org/" target="_blank" rel="noopener">Create Project</a><br />
+  </WelcomeItem>
+
+  <WelcomeItem>
+    <template #icon>
+      <ToolingIcon />
+    </template>
+    <template #heading>Activities</template>
+
+    <a href="https://vitejs.dev/guide/features.html" target="_blank" rel="noopener"
+      >Create activity</a
+    ><br />
+  </WelcomeItem>
+
+  <WelcomeItem>
+    <template #icon>
+      <EcosystemIcon />
+    </template>
+    <template #heading>Client</template>
+
+    <a href="https://pinia.vuejs.org/" target="_blank" rel="noopener">Create Project</a>
+  </WelcomeItem>
+</template>
---
+++ vue-project/src/views/ListProjects.vue
@@ -0,0 +1,48 @@
+<template>
+  <div class="projectList">
+    <h1>Project List goes here</h1>
+    <h3>
+      <ul>
+        <li>{{ projectList.id }}</li>
+        <li>{{ projectList.name }}</li>
+      </ul>
+    </h3>
+  </div>
+</template>
+
+<style>
+@media (min-width: 1024px) {
+  .projectList {
+    min-height: 100vh;
+    align-items: center;
+  }
+}
+</style>
+
+<script>
+import axios from 'axios'
+
+export default {
+  name: 'projectList',
+  data() {
+    return {
+      projectList: {}
+    }
+  },
+  created() {
+    this.getProjectList()
+  },
+  methods: {
+    getProjectList() {
+      axios
+        .get('http://localhost:8080/api/v1.0/practice/client/id/1')
+        .then((res) => {
+          this.projectList = res.data
+        })
+        .catch((error) => {
+          console.log(error)
+        })
+    }
+  }
+}
+</script>
